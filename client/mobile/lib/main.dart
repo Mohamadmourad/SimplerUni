6b0c0db32cda2682aab4aa3ff9edb5e0b1320ad7
waelmokhallalate <waelmok23@gmail.com>
2025-03-02 12:46:59 +0200

    SNP-3 Add Signup screen and update routing in main.dart

--- client/mobile/lib/main.dart
+++ client/mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:senior_project/screens/auth/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,25 +8,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+  
     final GoRouter router = GoRouter(
       initialLocation: '/',
       routes: [
         GoRoute(
           path: '/',
-          builder: (context, state) => const HomeScreen(),
+          builder: (context, state) => const Signup(),
         ),
         // GoRoute(
         //   path: '/second',
         //   builder: (context, state) => const SecondScreen(),
         // ),
+
       ],
     );
 
     return MaterialApp.router(
       routerConfig: router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
