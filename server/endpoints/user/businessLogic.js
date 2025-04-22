e738e84b6af378514e2ba44cffb9ddfd5531e96b
Mohamad Mourad <89941937+Mohamadmourad@users.noreply.github.com>
2025-04-22 11:27:10 +0300

    add profile picture cdn

--- server/endpoints/user/businessLogic.js
+++ server/endpoints/user/businessLogic.js
@@ -116,6 +116,9 @@ module.exports.addAdditionalUserData = async (req, res)=>{
         if(optionalData.bio){
             await db.query(`UPDATE users SET bio = $1 WHERE userid = $2`, [optionalData.bio, userId]); 
         }
+        if(optionalData.profilePicture){
+            await db.query(`UPDATE users SET profilePicture = $1 WHERE userid = $2`, [optionalData.profilePicture, userId]); 
+        }
         return res.status(200).json("data added succesfully");
     }
     catch(e){
