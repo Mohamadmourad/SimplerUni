f5dfc2d46673316ea4238759947093a01df9e560
Mohamad Mourad <89941937+Mohamadmourad@users.noreply.github.com>
2025-05-02 20:24:48 +0300

    minor fixes

--- server/endpoints/club/routes.js
+++ server/endpoints/club/routes.js
@@ -9,7 +9,7 @@ router.get("/acceptedClubs", clubController.getAcceptedClubs);
 router.get("/getClubsUserNotIn", clubController.getClubsUserNotIn);  
 router.get("/getClubsUserIsIn", clubController.getClubsUserIsIn);        
 router.get("/getAdminClubList", clubController.getAdminClubList);
-router.get("/getClubMembers", clubController.getClubMembers);
+router.get("/getClubMembers/:clubId", clubController.getClubMembers);
 
 router.post("/makeClubRequest", clubController.makeClubRequest);         
 router.post("/acceptClubRequest", clubController.acceptClubRequest);  
@@ -20,6 +20,6 @@ router.post("/rejectJoinRequest", clubController.rejectJoinRequest);
 
 router.put("/changeClubAdmin", clubController.changeClubAdmin);
 
-router.delete("/removerStudentFromClub", clubController.removerStudentFromClub);
+router.delete("/removerStudentFromClub/:clubId/:userId", clubController.removerStudentFromClub);
 
 module.exports = router;
